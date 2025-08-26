20c7260f3c07baa31756861f98ae5c88ae9a1e0b
SAHIL RAJA <sraza1692@gmail.com>
2025-08-26 23:34:24 +0530

    Add files via upload

---
+++ Matrices.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Matrices{
+
+    public static boolean search(int matrix[][], int key){
+        for(int i = 0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j] == key){ //checking key no with matrix
+                    System.out.println("(" + i + "," + j + ")");
+                    return true; // if found
+                }
+            }
+        }
+        System.out.println(" KEY NOT FOUND! ");
+        return false;
+    }
+    public static void main (String args[]){
+        int matrix[][] = new int[3][3];
+        int n = matrix.length, m = matrix[0].length;
+        // input
+        System.out.println("Enter Matrix of a Number:");
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        // output
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        search(matrix, 5);
+    }
+}
